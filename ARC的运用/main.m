efc8d52a1695fa1461298bdadfbe87f527f6048d
ArnoVD97 <784285777@qq.com>
2023-03-13 18:47:04 +0800

    Initial Commit

---
+++ ARC的运用/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ARC的运用
+//
+//  Created by zzy on 2023/3/13.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
