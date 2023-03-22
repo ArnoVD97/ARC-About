b2c212a8db8005608fb68df07cbad02d083bf272
ArnoVD97 <784285777@qq.com>
2023-03-22 14:41:52 +0800

    ARCgitBranchFirst

--- ARC的运用/main.m
+++ ARC的运用/main.m
@@ -28,7 +28,7 @@
 
 
 //测试二：
-/*int main(int argc, const char * argv[]) {
+int main(int argc, const char * argv[]) {
     @autoreleasepool {
         Person *p = [[Person alloc] init];//有一个强指针指向的person对象
         Person *p2 = p;//p2也指向person对象，此时有两个强指针指向
@@ -38,28 +38,28 @@
     //所以对象会在程序结束之后释放
     return 0;
 }
-*/
+
 
 
 
 
 //测试三：接下来来看弱指针的指向
-int main(int argc, const char * argv[]) {
-    
-        Person *p = [[Person alloc] init];
-        __weak Person *p2 = p;//声明一个弱指针，指向person对象
-    
-        p = nil;//把强指针变为空，对象没有强指针指向就会被释放
-    //只要强指针指向的对象不存在，ARC就会自动将指向该对象的弱指针清空，这样是防止野指针错误，该对象依然会被释放。不过好在，对象在被释放的同时，指向它的弱引用会自动被置nil，这个技术叫zeroing weak pointer。这样有效得防止无效指针、野指针的产生。__weak一般用在delegate关系中防止循环引用或者用来修饰指向由Interface Builder编辑与生成的UI控件。
-    
-        p2 = nil;//弱指针不能决定对象的释放
-        NSLog(@"--------");
-    //如果我们没有注释掉“强指针置空”，则会在程序结束前首先调用dealloc函数
-    //如果注释掉，则在程序结束前调用dealloc函数
-    
-        return 0;
-    
-}
+//int main(int argc, const char * argv[]) {
+//    
+//        Person *p = [[Person alloc] init];
+//        __weak Person *p2 = p;//声明一个弱指针，指向person对象
+//    
+//        p = nil;//把强指针变为空，对象没有强指针指向就会被释放
+//    //只要强指针指向的对象不存在，ARC就会自动将指向该对象的弱指针清空，这样是防止野指针错误，该对象依然会被释放。不过好在，对象在被释放的同时，指向它的弱引用会自动被置nil，这个技术叫zeroing weak pointer。这样有效得防止无效指针、野指针的产生。__weak一般用在delegate关系中防止循环引用或者用来修饰指向由Interface Builder编辑与生成的UI控件。
+//    
+//        p2 = nil;//弱指针不能决定对象的释放
+//        NSLog(@"--------");
+//    //如果我们没有注释掉“强指针置空”，则会在程序结束前首先调用dealloc函数
+//    //如果注释掉，则在程序结束前调用dealloc函数
+//    
+//        return 0;
+//    
+//}
 /*
  至于什么时候用strong 和weak？
 
